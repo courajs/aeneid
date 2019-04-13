6b0fb6f162c6208957bf55d235595bc84e0cbd51
Aaron Sikes <aaron@sikes.io>
2019-04-13 09:37:40 -0400

    Pull out shape grouping into a lib file

---
+++ app/lib/urbit/symbols.js
@@ -0,0 +1,76 @@
+import sylgraph from './sigil/lib/sylgraph';
+
+const pre = `
+dozmarbinwansamlitsighidfidlissogdirwacsabwissib\
+rigsoldopmodfoglidhopdardorlorhodfolrintogsilmir\
+holpaslacrovlivdalsatlibtabhanticpidtorbolfosdot\
+losdilforpilramtirwintadbicdifrocwidbisdasmidlop\
+rilnardapmolsanlocnovsitnidtipsicropwitnatpanmin\
+ritpodmottamtolsavposnapnopsomfinfonbanmorworsip\
+ronnorbotwicsocwatdolmagpicdavbidbaltimtasmallig\
+sivtagpadsaldivdactansidfabtarmonranniswolmispal\
+lasdismaprabtobrollatlonnodnavfignomnibpagsopral\
+bilhaddocridmocpacravripfaltodtiltinhapmicfanpat\
+taclabmogsimsonpinlomrictapfirhasbosbatpochactid\
+havsaplindibhosdabbitbarracparloddosbortochilmac\
+tomdigfilfasmithobharmighinradmashalraglagfadtop\
+mophabnilnosmilfopfamdatnoldinhatnacrisfotribhoc\
+nimlarfitwalrapsarnalmoslandondanladdovrivbacpol\
+laptalpitnambonrostonfodponsovnocsorlavmatmipfip\
+`
+
+const suf = `
+zodnecbudwessevpersutletfulpensytdurwepserwylsun\
+rypsyxdyrnuphebpeglupdepdysputlughecryttyvsydnex\
+lunmeplutseppesdelsulpedtemledtulmetwenbynhexfeb\
+pyldulhetmevruttylwydtepbesdexsefwycburderneppur\
+rysrebdennutsubpetrulsynregtydsupsemwynrecmegnet\
+secmulnymtevwebsummutnyxrextebfushepbenmuswyxsym\
+selrucdecwexsyrwetdylmynmesdetbetbeltuxtugmyrpel\
+syptermebsetdutdegtexsurfeltudnuxruxrenwytnubmed\
+lytdusnebrumtynseglyxpunresredfunrevrefmectedrus\
+bexlebduxrynnumpyxrygryxfeptyrtustyclegnemfermer\
+tenlusnussyltecmexpubrymtucfyllepdebbermughuttun\
+bylsudpemdevlurdefbusbeprunmelpexdytbyttyplevmyl\
+wedducfurfexnulluclennerlexrupnedlecrydlydfenwel\
+nydhusrelrudneshesfetdesretdunlernyrsebhulryllud\
+remlysfynwerrycsugnysnyllyndyndemluxfedsedbecmun\
+lyrtesmudnytbyrsenwegfyrmurtelreptegpecnelnevfes\
+`
+
+export const prefixes = pre.match(/.{3}/g);
+export const suffixes = suf.match(/.{3}/g);
+
+let symbols = [];
+let bases = {};
+for (let i = 0; i <= 0xff; i++) {
+  let syl = prefixes[i];
+  let ref = sylgraph.mapping[syl];
+  let base = ref.split(':')[0];
+  let s = {
+    value: i,
+    prefix: true,
+    syl, ref, base,
+  };
+  symbols.push(s);
+  bases[base] = bases[base] || {ref: base, forms: []};
+  bases[base].forms.push(s);
+
+
+  syl = suffixes[i];
+  ref = sylgraph.mapping[syl];
+  base = ref.split(':')[0];
+  s = {
+    value: i,
+    suffix: true,
+    syl, ref, base,
+  };
+  symbols.push(s);
+  bases[base] = bases[base] || {ref: base, forms: []};
+  bases[base].forms.push(s);
+}
+
+export default {
+  symbols,
+  shapes: Object.values(bases),
+};
